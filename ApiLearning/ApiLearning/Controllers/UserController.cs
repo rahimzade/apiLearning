e04f96579496f3942ae69a85222c6d0ef64090b2
rahimzade <40488188+rahimzade@users.noreply.github.com>
2022-12-13 22:07:47 +0330

    Add files via upload

---
+++ ApiLearning/ApiLearning/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using ApiLearning.model.exam;
+using ApiLearning.model.UserList;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLearning.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController
+    {
+
+        [HttpPost]
+        public bool Register(UserInfo user)
+        {
+            try
+            {
+                // UsersList UserList = new UsersList();
+                //users.Add(user);
+                UsersList.users.Add(user);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+       
+    }
+}
+
---
+++ ApiLearning/ApiLearning/model/UserList/UsersList.cs
@@ -0,0 +1,11 @@
+﻿using ApiLearning.model.exam;
+
+namespace ApiLearning.model.UserList
+{
+    public static class UsersList
+    {
+        public static List<UserInfo> users = new List<UserInfo>();
+
+
+    }
+}
